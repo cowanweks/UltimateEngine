c35dcd11f64a1f29d84480fc8587837549c6c247
cowanweks <cowannwekesa@gmail.com>
2023-11-27 03:31:54 +0300

    Added CMAKELists

---
+++ build/Editor/CMakeFiles/UltimateEngine.dir/DependInfo.cmake
@@ -0,0 +1,60 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "RC"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_RC
+  "E:/Projects/UltimateEngine/Editor/resources/media.rc" "E:/Projects/UltimateEngine/build/Editor/CMakeFiles/UltimateEngine.dir/resources/media.rc.obj"
+  )
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_RC
+  "_DEBUG"
+  )
+
+# The include file search paths:
+set(CMAKE_RC_TARGET_INCLUDE_PATH
+  "E:/Projects/UltimateEngine/Editor/include"
+  "E:/Projects/UltimateEngine/Editor/sources/**/include"
+  "E:/Projects/UltimateEngine/Editor/libs/entt/include"
+  "E:/Projects/UltimateEngine/Editor/libs/imgui/include"
+  "E:/Projects/UltimateEngine/Editor/libs/glfw/include"
+  "E:/Projects/UltimateEngine/Editor/libs/filewatch/include"
+  "E:/Projects/UltimateEngine/Editor/libs/glm"
+  "E:/Projects/UltimateEngine/Editor/libs/spdlog/include"
+  "E:/Projects/UltimateEngine/Editor/libs/stb_image"
+  "E:/Projects/UltimateEngine/Editor/libs/yaml-cpp"
+  "/include"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "E:/Projects/UltimateEngine/Editor/libs/imgui/src/imgui.cpp" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/libs/imgui/src/imgui_demo.cpp" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_demo.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_demo.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/libs/imgui/src/imgui_draw.cpp" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_draw.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_draw.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/libs/imgui/src/imgui_impl_dx11.cpp" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_impl_dx11.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_impl_dx11.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/libs/imgui/src/imgui_impl_glfw.cpp" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_impl_glfw.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_impl_glfw.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/libs/imgui/src/imgui_impl_opengl2.cpp" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_impl_opengl2.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_impl_opengl2.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/libs/imgui/src/imgui_impl_opengl3.cpp" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_impl_opengl3.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_impl_opengl3.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/libs/imgui/src/imgui_impl_win32.cpp" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_impl_win32.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_impl_win32.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/libs/imgui/src/imgui_tables.cpp" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_tables.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_tables.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/libs/imgui/src/imgui_widgets.cpp" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_widgets.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/libs/imgui/src/imgui_widgets.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/sources/App.cpp" "Editor/CMakeFiles/UltimateEngine.dir/sources/App.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/sources/App.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/sources/Logger.cpp" "Editor/CMakeFiles/UltimateEngine.dir/sources/Logger.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/sources/Logger.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/sources/Splash.cpp" "Editor/CMakeFiles/UltimateEngine.dir/sources/Splash.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/sources/Splash.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/sources/UEditor.cpp" "Editor/CMakeFiles/UltimateEngine.dir/sources/UEditor.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/sources/UEditor.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/sources/Window.cpp" "Editor/CMakeFiles/UltimateEngine.dir/sources/Window.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/sources/Window.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/sources/platform/directx/platform_dx.cpp" "Editor/CMakeFiles/UltimateEngine.dir/sources/platform/directx/platform_dx.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/sources/platform/directx/platform_dx.cpp.obj.d"
+  "E:/Projects/UltimateEngine/Editor/sources/platform/vulkan/platform_vk.cpp" "Editor/CMakeFiles/UltimateEngine.dir/sources/platform/vulkan/platform_vk.cpp.obj" "gcc" "Editor/CMakeFiles/UltimateEngine.dir/sources/platform/vulkan/platform_vk.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/Editor/CMakeFiles/UltimateEngine.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for UltimateEngine.
+# This may be replaced when dependencies are built.
